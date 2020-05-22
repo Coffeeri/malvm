4d337828f58ef149c7b07a7e1b85d4daa9325b30
Leander Kohler <leander.kohler@uni-bonn.de>
2020-05-22 16:04:29 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 """This file contains package details."""
 import setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
-    name="malvm", # Replace with your own username
+    name="malvm",
     version="0.0.1",
-    install_requires=requirements,
+    install_requires=["click"],
     author="Leander Kohler",
     author_email="leander.kohler@uni-bonn.de",
     description="Creates non detectable VMs to analyze Malware.",
     url="https://gitlab.com/shk_fkie/analysevm/",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
-    python_requires='>=3.6',
+    classifiers=["Programming Language :: Python :: 3",],
+    python_requires=">=3.6",
 )

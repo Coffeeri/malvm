c0f0a1d59b9e37a78ff3f6267eb81aff13c12adf
Leander <leander.kohler@uni-bonn.de>
2020-06-05 15:15:53 +0200

    Test pytest in Gitlab CI
    + compiling approach to fix "File not found ...json"

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     package_dir={"malvm": "src/malvm"},
     install_requires=["click"],
-    package_data={"": ["data/*.json"]},
+    package_data={"": ["*.json"]},
     entry_points={"console_scripts": ["malvm = malvm.__main__:main"]},
 )

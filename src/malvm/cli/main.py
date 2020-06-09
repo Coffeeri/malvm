4ecb0b4fcaaf53adf7e8fcc41bd529191e43d89b
Leander Kohler <leander.kohler@uni-bonn.de>
2020-06-09 12:59:39 +0200

    Add ability to create Malboxes
    + CLI addon
    + added examples in README.md

--- src/malvm/cli/main.py
+++ src/malvm/cli/main.py
@@ -1,10 +1,7 @@
-import sys
-
 import click
 
-from malvm.controller import Controller
-
-controller: Controller = Controller()
+from .malboxes.malboxes import box
+from .malvm.malvm import show, check, fix
 
 
 @click.group()
@@ -12,93 +9,7 @@ def malvm():
     """Base CLI-command for malvm."""
 
 
-def return_bool_styled(return_value: bool) -> str:
-    """Returns a styled version of check return values.
-
-    Returns:
-        str: Click-styled value.
-    """
-    return_value_styled = (
-        click.style("OK.", fg="green")
-        if return_value
-        else click.style("NEEDS FIX.", fg="red")
-    )
-    return return_value_styled
-
-
-@malvm.command()
-@click.argument("characteristic", required=False)
-def check(characteristic: str) -> None:
-    """Checks satisfaction of CHARACTERISTIC.
-
-    [characteristic-code] for checking specific characteristic.
-    All characteristics will be checked if not mentioned.
-    """
-    if characteristic:
-        try:
-            for slug, _, value, status in controller.run_check(characteristic.upper()):
-                return_value_styled = return_bool_styled(status)
-                click.echo(
-                    f"[{click.style(slug, fg='yellow')}] "
-                    f"{value} "
-                    f"{return_value_styled}"
-                )
-        except ValueError as error_value:
-            click.echo(click.style(str(error_value), fg="red"))
-            sys.exit(1)
-
-    else:
-        for slug, _, value, status in controller.run_checks():
-            return_value_styled = return_bool_styled(status)
-            click.echo(
-                f"[{click.style(slug, fg='yellow')}] "
-                f"{value} "
-                f"{return_value_styled}"
-            )
-
-
-@malvm.command()
-@click.argument("characteristic", required=False)
-def fix(characteristic: str) -> None:
-    """Fixes satisfaction of CHARACTERISTIC."""
-    if characteristic:
-        try:
-            for slug, _, value, status in controller.run_fix(characteristic.upper()):
-                return_value_styled = return_bool_styled(status)
-                click.echo(
-                    f"[{click.style(slug, fg='yellow')}] "
-                    f"{value} "
-                    f"{return_value_styled}"
-                )
-        except ValueError as error_value:
-            click.echo(click.style(str(error_value), fg="red"))
-            sys.exit(1)
-
-    else:
-        for slug, _, value, status in controller.run_fixes():
-            return_value_styled = return_bool_styled(status)
-            click.echo(
-                f"[{click.style(slug, fg='yellow')}] "
-                f"{value} "
-                f"{return_value_styled}"
-            )
-
-
-@malvm.command()
-@click.option("-a", "--show-all", "show_all", type=bool, is_flag=True, default=False)
-def show(show_all: bool) -> None:
-    """Lists all characteristics.
-
-    Args:
-        show_all: Returns a list of all characteristics, including sub characteristics.
-    """
-    characteristic_list = (
-        controller.get_characteristic_list_all()
-        if show_all
-        else controller.get_characteristic_list()
-    )
-    for characteristic in characteristic_list:
-        click.echo(
-            f"[{click.style(characteristic.slug, fg='yellow')}] "
-            f"{characteristic.description}"
-        )
+malvm.add_command(show)
+malvm.add_command(check)
+malvm.add_command(fix)
+malvm.add_command(box)
---
+++ src/malvm/cli/malvm/malvm.py
@@ -0,0 +1,86 @@
+import sys
+
+import click
+
+from ...controller import Controller
+from .utils import return_bool_styled
+
+controller: Controller = Controller()
+
+
+@click.command()
+@click.argument("characteristic", required=False)
+def check(characteristic: str) -> None:
+    """Checks satisfaction of CHARACTERISTIC.
+
+    [characteristic-code] for checking specific characteristic.
+    All characteristics will be checked if not mentioned.
+    """
+    if characteristic:
+        try:
+            for slug, _, value, status in controller.run_check(characteristic.upper()):
+                return_value_styled = return_bool_styled(status)
+                click.echo(
+                    f"[{click.style(slug, fg='yellow')}] "
+                    f"{value} "
+                    f"{return_value_styled}"
+                )
+        except ValueError as error_value:
+            click.echo(click.style(str(error_value), fg="red"))
+            sys.exit(1)
+
+    else:
+        for slug, _, value, status in controller.run_checks():
+            return_value_styled = return_bool_styled(status)
+            click.echo(
+                f"[{click.style(slug, fg='yellow')}] "
+                f"{value} "
+                f"{return_value_styled}"
+            )
+
+
+@click.command()
+@click.argument("characteristic", required=False)
+def fix(characteristic: str) -> None:
+    """Fixes satisfaction of CHARACTERISTIC."""
+    if characteristic:
+        try:
+            for slug, _, value, status in controller.run_fix(characteristic.upper()):
+                return_value_styled = return_bool_styled(status)
+                click.echo(
+                    f"[{click.style(slug, fg='yellow')}] "
+                    f"{value} "
+                    f"{return_value_styled}"
+                )
+        except ValueError as error_value:
+            click.echo(click.style(str(error_value), fg="red"))
+            sys.exit(1)
+
+    else:
+        for slug, _, value, status in controller.run_fixes():
+            return_value_styled = return_bool_styled(status)
+            click.echo(
+                f"[{click.style(slug, fg='yellow')}] "
+                f"{value} "
+                f"{return_value_styled}"
+            )
+
+
+@click.command()
+@click.option("-a", "--show-all", "show_all", type=bool, is_flag=True, default=False)
+def show(show_all: bool) -> None:
+    """Lists all characteristics.
+
+    Args:
+        show_all: Returns a list of all characteristics, including sub characteristics.
+    """
+    characteristic_list = (
+        controller.get_characteristic_list_all()
+        if show_all
+        else controller.get_characteristic_list()
+    )
+    for characteristic in characteristic_list:
+        click.echo(
+            f"[{click.style(characteristic.slug, fg='yellow')}] "
+            f"{characteristic.description}"
+        )

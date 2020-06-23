e3a1e42dd5daa43158c513f11824f10b91bb2e3b
Leander <leander.kohler@uni-bonn.de>
2020-06-23 10:30:36 +0200

    Add registry characteristic
    + VirtualBox registry entry data in registry.json
    + a registry task contains an action (REMOVE | CHANGE)
    + a key can be removed recursively or
    + a parameter of a key can be changed/ created

--- src/malvm/characteristics/registry/registry_task.py
+++ src/malvm/characteristics/registry/registry_task.py
@@ -3,22 +3,131 @@
 Classes:
     RegistryVBCharacteristic: Checks and Fixes registry entries referring to VirtualBox.
 """
+from enum import Enum
 from pathlib import Path
-from typing import List, Dict, Tuple
-from _winreg import *
+from typing import List, Dict, Tuple, NamedTuple, Union
+from winreg import (
+    HKEY_LOCAL_MACHINE,
+    OpenKey,
+    KEY_ALL_ACCESS,
+    QueryInfoKey,
+    EnumKey,
+    DeleteKey,
+    KEY_WOW64_64KEY,
+    KEY_WOW64_32KEY,
+    SetValue, REG_SZ, QueryValueEx, CreateKey, SetValueEx)
 
 from ..abstract_characteristic import Characteristic, LambdaCharacteristic
 from ...utils.helper_methods import get_project_root, read_json_file
 
 
-def check_reg_key(key: str) -> bool:
-    base_key = None
+class RegistryAction(Enum):
+    REMOVE = 1
+    CHANGE = 2
+
+
+class RegistryTask(NamedTuple):
+    slug: str
+    action: RegistryAction
+    hypervisor: str
+    key: str
+    parameter: str
+    value: str
+
+
+def check_registry_key(task: RegistryTask) -> bool:
+    """Checks if registry task is already satisfied."""
+    if task.action == RegistryAction.REMOVE:
+        return not check_registry_key_exists(task)
+    elif task.action == RegistryAction.CHANGE:
+        return check_registry_key_value(task)
+    return False
+
+
+def check_registry_key_exists(task: RegistryTask) -> bool:
+    """Returns True if key exists in registry."""
+    try:
+        key_base, key_path = split_key(task.key)
+        OpenKey(key_base, key_path)
+    except FileNotFoundError:
+        return False
+    return True
+
+
+def check_registry_key_value(task: RegistryTask) -> bool:
+    """Returns true if the value of key is set in registry."""
+    key_base, key_path = split_key(task.key)
+    try:
+        key = OpenKey(key_base, key_path)
+        key_value = QueryValueEx(key, task.parameter)[0]
+        key_value = normalize_key_value(key_value)
+        if key_value == task.value:
+            return True
+    except FileNotFoundError:
+        return False
+    return False
+
+
+def normalize_key_value(key_value: Union[str, List[str]]) -> str:
+    """Returns normalize string of registry key value."""
+    if isinstance(key_value, List):
+        key_value = "\n".join(key_value)
+    return key_value
+
+
+def split_key(key: str) -> Tuple[int, str]:
+    """Returns key-base and remaining key-path."""
+    key_base = None
+    key_path = key
     if key.startswith("HKEY_LOCAL_MACHINE\\"):
-        base_key = HKEY_LOCAL_MACHINE
+        key_base = HKEY_LOCAL_MACHINE
+        key_path = key_path.replace("HKEY_LOCAL_MACHINE\\", "")
+    return key_base, key_path
 
 
-def fix_reg_key(key_value: Tuple[str, str]) -> bool:
-    pass
+def fix_registry_key(task: RegistryTask) -> bool:
+    """Fixes registry key entry."""
+    if task.action == RegistryAction.REMOVE:
+        remove_key(task)
+    elif task.action == RegistryAction.CHANGE:
+        set_key_value(task)
+    return check_registry_key(task)
+
+
+def set_key_value(task: RegistryTask):
+    """Sets value of key.
+
+    If the key does not exist, it will be created.
+    Only strings are supported.
+    """
+    key_base, key_path = split_key(task.key)
+    key = CreateKey(key_base, key_path)
+    SetValueEx(key, task.parameter, 0, REG_SZ, task.value)
+
+
+def remove_key(task: RegistryTask) -> None:
+    """Removes key recursively in 32Bit and 64Bit mode."""
+    key_base, key_path = split_key(task.key)
+    arch_keys = [KEY_WOW64_32KEY, KEY_WOW64_64KEY]
+    for arch_key in arch_keys:
+        try:
+            delete_sub_key(key_base, key_path, arch_key)
+        except OSError as e:
+            print(e)
+
+
+def delete_sub_key(key0: int, current_key: str, arch_key: int = 0) -> None:
+    """Deletes registry key including all sub-keys."""
+    open_key = OpenKey(key0, current_key, 0, KEY_ALL_ACCESS | arch_key)
+    info_key = QueryInfoKey(open_key)
+    for x in range(0, info_key[0]):
+        sub_key = EnumKey(open_key, 0)
+        try:
+            DeleteKey(open_key, sub_key)
+        except OSError:
+            delete_sub_key(key0, "\\".join([current_key, sub_key]), arch_key)
+    DeleteKey(open_key, "")
+    open_key.Close()
 
 
 def sub_characteristics_virtualbox() -> List[LambdaCharacteristic]:
@@ -27,9 +136,21 @@ def sub_characteristics_virtualbox() -> List[LambdaCharacteristic]:
     data: List[Dict[str, str]] = read_json_file(data_path)
     registry_characteristics: List[LambdaCharacteristic] = []
     for entry in data:
-        description = f"Registry entry identifying VirtualBox {entry['key']}"
+        registry_task = RegistryTask(
+            slug=entry["slug"],
+            action=RegistryAction[entry["action"].upper()],
+            hypervisor=entry["hypervisor"],
+            key=entry["key"],
+            parameter=None if "parameter" not in entry else entry["parameter"],
+            value=None if "value" not in entry else entry["value"],
+        )
+        description = f"Registry entry identifying VirtualBox {registry_task.key}"
         characteristic = LambdaCharacteristic(
-            entry["slug"], description, entry["key"], check_reg_key, fix_reg_key
+            registry_task.slug,
+            description,
+            registry_task,
+            check_registry_key,
+            fix_registry_key,
         )
         registry_characteristics.append(characteristic)
     return registry_characteristics

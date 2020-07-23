345fff4ec239e2fac3224e0c0f0607af0d18c352
Leander Kohler <leander.kohler@uni-bonn.de>
2020-07-23 16:17:02 +0200

    Add packer files for windows 10

---
+++ src/malvm/data/packer/scripts/install_malvm.bat
@@ -0,0 +1,4 @@
+cd C:/tmp/analysevm
+pip install -r requirements.txt
+python setup.py install
+malvm fix
---
+++ src/malvm/data/packer/scripts/install_malvm_dependencies.bat
@@ -0,0 +1,4 @@
+:: set /p PATH=%PATH%;C:\ProgramData\chocolatey\
+echo %PATH%
+choco install /y python3 git.install
+

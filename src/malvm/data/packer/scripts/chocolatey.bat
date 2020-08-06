acc5004c3c55acc4ef56462a8eb0945358e87720
Leander Kohler <leander.kohler@uni-bonn.de>
2020-08-06 18:59:26 +0200

    Update chocolatey install command

--- src/malvm/data/packer/scripts/chocolatey.bat
+++ src/malvm/data/packer/scripts/chocolatey.bat
@@ -1,1 +1,1 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))

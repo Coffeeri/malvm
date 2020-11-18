3a0b07177b0238bc5cc0d3cdd4ae1a9e21cb47c2
Leander Kohler <leander.kohler@uni-bonn.de>
2020-11-18 14:58:36 +0100

    Refactor FileCharacteristic

--- src/malvm/characteristics/files/file_task.py
+++ src/malvm/characteristics/files/file_task.py
@@ -5,10 +5,17 @@ Classes:
     FileVMWCharacteristic: Checks and Fixes for files referring to VMWare.
 """
 from pathlib import Path
-from typing import List
+from typing import List, Iterator
 
 from ...utils.helper_methods import get_project_root, read_json_file
 from ..abstract_characteristic import Characteristic, LambdaCharacteristic
+
+
+class FileVBCharacteristic(Characteristic):
+    def __init__(self) -> None:
+        super().__init__("FVB", "Files identifying VirtualBox.")
+        sub_characteristics = sub_characteristics_virtualbox()
+        self.add_sub_characteristic_list(list(sub_characteristics))
 
 
 def check_path_not_exists(path: str) -> bool:
@@ -17,22 +24,15 @@ def check_path_not_exists(path: str) -> bool:
 
 
 def remove_path(path: str) -> bool:
-    """Removes file or directory.
-
-    After removing the file/ directory, a path check is executed to validated the
-    success of removal.
-
-    Args:
-        path(str): Path to file/ directory.
+    """Removes file or directory by path-string.
 
     Returns:
-        bool: Returns True if file was deleted correctly.
+        bool: Returns True if file was deleted successfully.
     """
     if check_path_not_exists(path):
         return True
     path_obj = Path(path)
     try:
-
         if path_obj.is_file():
             path_obj.unlink()
         elif path_obj.is_dir():
@@ -42,65 +42,24 @@ def remove_path(path: str) -> bool:
     return check_path_not_exists(path)
 
 
-def get_virtualbox_files():
-    """Returns all files identifying VirtualBoxs existence."""
+def get_virtualbox_files() -> List[List[str]]:
+    """Returns all files identifying VirtualBox existence."""
     return read_json_file(
         Path(Path(get_project_root() / "data/files_virtualbox.json")).absolute()
     )["files"]
 
 
-def get_vmware_files():
-    """Returns all files identifying VirtualBoxs existence."""
-    return read_json_file(
-        Path(Path(get_project_root() / "data/files_vmware.json")).absolute()
-    )["files"]
-
-
-def sub_characteristics_virtualbox() -> List[LambdaCharacteristic]:
+def sub_characteristics_virtualbox() -> Iterator[LambdaCharacteristic]:
     """Creates a list of LambdaCharacteristics.
 
-    A new sub characteristic will be created for each File.
+    A new sub characteristic will be created for each file.
     """
-    file_characteristics: List[LambdaCharacteristic] = []
-    data: List[List[str]] = get_virtualbox_files()
-    for slug, file_path in data:
-        description = f"File identifying VirtualBox existence: {file_path}"
+    for slug, file_path in get_virtualbox_files():
         characteristic = LambdaCharacteristic(
-            slug, description, file_path, check_path_not_exists, remove_path,
+            slug=slug,
+            description=f"File identifying VirtualBox existence: {file_path}",
+            value=file_path,
+            check_func=check_path_not_exists,
+            fix_func=remove_path,
         )
-        file_characteristics.append(characteristic)
-    return file_characteristics
-
-
-def sub_characteristics_vmware() -> List[LambdaCharacteristic]:
-    """Creates a list of LambdaCharacteristics.
-
-    A new sub characteristic will be created for each File.
-    """
-    file_characteristics: List[LambdaCharacteristic] = []
-    data: List[List[str]] = get_vmware_files()
-    for slug, file_path in data:
-        description = f"File identifying VMWare existence: {file_path}"
-        characteristic = LambdaCharacteristic(
-            slug, description, file_path, check_path_not_exists, remove_path,
-        )
-        file_characteristics.append(characteristic)
-    return file_characteristics
-
-
-class FileVBCharacteristic(Characteristic):
-    """Checks and Fixes for existence of striking files."""
-
-    def __init__(self) -> None:
-        super().__init__("FVB", "Files identifying VirtualBox.")
-        sub_characteristics = sub_characteristics_virtualbox()
-        self.add_sub_characteristic_list(list(sub_characteristics))
-
-
-class FileVMWCharacteristic(Characteristic):
-    """Checks and Fixes for existence of striking files."""
-
-    def __init__(self) -> None:
-        super().__init__("FVMW", "Files identifying VMWare.")
-        sub_characteristics = sub_characteristics_vmware()
-        self.add_sub_characteristic_list(list(sub_characteristics))
+        yield characteristic

ddb6c3dfb82e09035ed550f33820d4c479e37fd7
Leander Kohler <leander.kohler@uni-bonn.de>
2020-11-18 16:39:12 +0100

    Refactor function order of controller.py

--- src/malvm/controller/controller.py
+++ src/malvm/controller/controller.py
@@ -76,46 +76,39 @@ class Controller(metaclass=SingletonMeta):
     def add_characteristic(self, characteristic: CharacteristicBase) -> None:
         self.characteristics[characteristic.slug] = characteristic
 
-    def __get_all_characteristics(self) -> Iterator[CharacteristicBase]:
-        """Returns all characteristics including all sub characteristics."""
-        for characteristic in self.characteristics.values():
-            yield characteristic
-            yield from get_sub_characteristics(characteristic)
-
     def __get_all_characteristics_dict(self) -> Dict[str, CharacteristicBase]:
         return {
             characteristic.slug: characteristic
             for characteristic in self.__get_all_characteristics()
         }
 
-    def get_characteristic_list(
-        self,
-        include_sub_characteristics: bool = False,
-        runtime: Optional[Runtime] = Runtime.DEFAULT,
-    ) -> List[CharacteristicBase]:
-        """Returns filtered characteristics.
+    def __get_all_characteristics(self) -> Iterator[CharacteristicBase]:
+        for characteristic in self.characteristics.values():
+            yield characteristic
+            yield from get_sub_characteristics(characteristic)
 
-        Args:
-            include_sub_characteristics (bool): Select True if sub characteristics
-                                                should be included.
-            runtime (Runtime): Enum, defining the time and location when check and fix
-                               methods will be executed.
-        """
-        if include_sub_characteristics:
-            characteristics = self.__get_all_characteristics()
-        else:
-            characteristics = iter(self.characteristics.values())
+    def get_all_checks_results(self) -> CheckResult:
+        for characteristic in self.get_characteristic_list():
+            yield from self.get_check_results(characteristic.slug)
 
-        return [
-            characteristic
-            for characteristic in characteristics
-            if ((characteristic.attributes.runtime == runtime) or (runtime is None))
-        ]
+    def get_pre_boot_checks_results(self, environment: Dict[str, Any]) -> CheckResult:
+        for characteristic in self.get_characteristic_list(False, Runtime.PRE_BOOT):
+            self.characteristics[characteristic.slug].environment = environment
+            yield from self.get_check_results(characteristic.slug)
 
     def get_check_results(self, slug_searched: str) -> CheckResult:
         yield from self.__action_on_characteristic_with_results(
             slug_searched, CharacteristicAction.CHECK
         )
+
+    def apply_all_fixes_get_results(self) -> CheckResult:
+        for characteristic in self.get_characteristic_list():
+            yield from self.apply_fix_get_results(characteristic.slug)
+
+    def apply_pre_boot_fixes(self, environment: Dict[str, Any]) -> CheckResult:
+        for characteristic in self.get_characteristic_list(False, Runtime.PRE_BOOT):
+            self.characteristics[characteristic.slug].environment = environment
+            yield from self.apply_fix_get_results(characteristic.slug)
 
     def apply_fix_get_results(self, slug_searched: str) -> CheckResult:
         yield from self.__action_on_characteristic_with_results(
@@ -131,20 +124,21 @@ class Controller(metaclass=SingletonMeta):
         else:
             raise ValueError("Characteristic was not found.")
 
-    def get_all_checks_results(self) -> CheckResult:
-        for characteristic in self.get_characteristic_list():
-            yield from self.get_check_results(characteristic.slug)
+    def get_characteristic_list(
+        self,
+        include_sub_characteristics: bool = False,
+        selected_runtime: Optional[Runtime] = Runtime.DEFAULT,
+    ) -> List[CharacteristicBase]:
+        if include_sub_characteristics:
+            characteristics = self.__get_all_characteristics()
+        else:
+            characteristics = iter(self.characteristics.values())
 
-    def apply_all_fixes_get_results(self) -> CheckResult:
-        for characteristic in self.get_characteristic_list():
-            yield from self.apply_fix_get_results(characteristic.slug)
-
-    def apply_pre_boot_fixes(self, environment: Dict[str, Any]) -> CheckResult:
-        for characteristic in self.get_characteristic_list(False, Runtime.PRE_BOOT):
-            self.characteristics[characteristic.slug].environment = environment
-            yield from self.apply_fix_get_results(characteristic.slug)
-
-    def get_pre_boot_checks_results(self, environment: Dict[str, Any]) -> CheckResult:
-        for characteristic in self.get_characteristic_list(False, Runtime.PRE_BOOT):
-            self.characteristics[characteristic.slug].environment = environment
-            yield from self.get_check_results(characteristic.slug)
+        return [
+            characteristic
+            for characteristic in characteristics
+            if (
+                (characteristic.attributes.runtime == selected_runtime)
+                or (selected_runtime is None)
+            )
+        ]

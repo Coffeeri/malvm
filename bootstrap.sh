bb328c414b2e3aec096a01da1afbd25eedbf954d
Leander Kohler <leander.kohler@uni-bonn.de>
2020-12-03 20:57:23 +0100

    Improve boostrap.sh
    + fancy printing
    + checks if venv exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-pip3 install virtualenv
-virtualenv -p python3.8 venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 setup.py install
+textBold=$(tput bold)
+fgYellow=$(tput setaf 3)
+fgWhite=$(tput setaf 7)
+textReset=$(tput sgr0)
+echo "${fgYellow}"
+if [ ! -d "venv" ]; then
+    echo "${textBold}Setting up environment.."
+    echo "${fgWhite}"
+    pip3 install virtualenv
+    virtualenv -p python3.8 venv
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    python3 setup.py sdist
+    python3 setup.py install
+else
+    echo "${textBold}Environment already exists."
+    echo "${textBold}Activate environment.."
+
+fi
+echo "${fgYellow}"
+. ./venv/bin/activate
+echo "${textBold}Have fun, bye."
+echo "${textReset}"

ed3859fa44d610855b430ec6ec56351bccf163e3
Leander Kohler <leander.kohler@uni-bonn.de>
2020-12-17 19:16:06 +0100

    Introduce basic logging

---
+++ src/malvm/utils/logging.py
@@ -0,0 +1,20 @@
+import logging
+from logging.handlers import RotatingFileHandler, SysLogHandler
+
+from .helper_methods import get_logfile_path
+
+logger = logging.getLogger("malvm")
+
+logger.setLevel(logging.DEBUG)
+file_handler = RotatingFileHandler(str(get_logfile_path() / "malvm.log"),
+                                   maxBytes=10000,
+                                   backupCount=10)
+syslog_handler = SysLogHandler(address='/dev/log')
+
+file_formatter = logging.Formatter(
+    '%(asctime)s:%(name)s:%(levelname)s: %(message)s')
+
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(syslog_handler)

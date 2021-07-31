f4199a638e6523e16c26ba6ae2ba6683f5ab8017
Leander Kohler <leander.kohler@uni-bonn.de>
2021-07-31 19:06:05 +0200

    Add FADT registry characteristic

--- src/malvm/characteristics/hardware/__init__.py
+++ src/malvm/characteristics/hardware/__init__.py
@@ -1,2 +1,2 @@
 """This package includes all Classes representing dmi hardware characteristics."""
-from .hardware_task import DMIHardwareCharacteristic, VBoxDeviceRemoval, DSDTRegistry  # noqa: F401
+from .hardware_task import DMIHardwareCharacteristic, VBoxDeviceRemoval, DSDTRegistry, FADTRegistry  # noqa: F401
